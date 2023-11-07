ffc9b3dcb586ee9bb5b2f4c9e9ff9105a0b2cbf4
parshuram16 <150114855+parshuram16@users.noreply.github.com>
2023-11-07 19:56:53 +0530

    Update ramulu.tf

--- ramulu/ramulu.tf
+++ ramulu/ramulu.tf
@@ -1,8 +1,24 @@
 provider "aws" {
-    region = "us-east-1"
+  region     = "us-east-1"
+  access_key = "AKIAS5N235KTSHBILSOO"
+  secret_key = "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"
 }
 
-resource "aws_instance" "parshi" {
-    ami           = "ami-05c13eab67c5d8861"
-    instance_type = "t2.micro"
+
+resource "aws_instance" "terraformeast1" {
+  ami                     = "ami-05c13eab67c5d8861"
+  instance_type           = "t2.micro"
+}
+
+provider "aws" {
+  region     = "us-east-2"
+  access_key = "AKIAS5N235KTSHBILSOO"
+  secret_key = "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"
+  alias="east"
+}
+
+resource "aws_instance" "terraforeast2" {
+  ami                     = "ami-089c26792dcb1fbd4"
+  instance_type           = "t2.micro"
+  provider="aws.east"
 }
