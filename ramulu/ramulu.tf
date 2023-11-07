384535dc0e9ef11b7707cd7549d62b3e797956f5
parshuram16 <150114855+parshuram16@users.noreply.github.com>
2023-11-07 19:06:57 +0530

    Update ramulu.tf

--- ramulu/ramulu.tf
+++ ramulu/ramulu.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "parshi" {
     ami           = "ami-05c13eab67c5d8861"
     instance_type = "t2.micro"
 }
